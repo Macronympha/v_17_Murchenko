602c0f39d1a1648188a2fa959639d3d7ff034371
Rina <irina.alman98@gmail.com>
2020-12-25 15:15:52 +0700

    first commit

---
+++ app/src/main/java/com/example/murchenko_v_17/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.murchenko_v_17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView bth = (ImageView) findViewById(R.id.imageView2);
+        bth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
